5ae0e1907e33b4d0723e0d5d9285a83b3799ed38
Lana <al.t.loureiro0@gmail.com>
2024-06-17 16:23:14 -0300

    slide show

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,32 @@ function closeCircuit() {
    modal.style.display = "none";
    document.body.style.overflow = "auto"; 
 }
+
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const newTransformValue = -currentSlide * 100 + '%';
+    document.querySelector('.carousel').style.transform = `translateX(${newTransformValue})`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+showSlide(currentSlide);
